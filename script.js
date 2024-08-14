2b1461738ccadd330c5aea035b0518a79edb1bc1
Gaurav Bhatt <154879303+Gauravbhatt035@users.noreply.github.com>
2024-08-14 12:55:24 +0530

    add css and javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+function updatecard(){
+  document.getElementById("School Name val").innerHTML= document.getElementById("School Name").value;
+   
+
+ document.getElementById("StudentNameval").innerHTML= document.getElementById("Student Name").value;
+   
+
+ document.getElementById("Date of Birth val").innerHTML= document.getElementById("Date of Birth").value;
+   
+
+  document.getElementById("Section val").innerHTML= document.getElementById("Section").value;
+  
+
+  document.getElementById("Roll Number val").innerHTML = document.getElementById("Rollnum").value;
+ 
+}
+
+function PrintID(){
+    window.print();
+}
